e7444af25fe5140275c265b3e8e240b99206a25e
Kris244212 <kyrylovets.k.m_kn24@rcit.ukr.education>
2025-10-16 23:06:36 +0300

    Додано скрін у репозиторій

--- obj/Debug/net8.0/Lab5v2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Lab5v2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab5v2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38cf5db22cbc8c0afcfe3dcc2622d4f90b2646b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67e22485b3ecf5bc41ca03ce1cfc1f65e9191779")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab5v2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab5v2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
